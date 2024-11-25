d3693cf0bb268233e73d6478642facb52bd152a2
Alexander Silva Lima <111926533+alexttz@users.noreply.github.com>
2024-11-25 19:11:30 -0300

    Add Telebot application

---
+++ Telebot/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        String botToken = "7323542526:AAFG7877Iu1qNIMlGSNWrgrNVO7jPI1quAk";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
+            System.out.println("OgrimmBot successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
